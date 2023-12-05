6a865031ff8eaa28998ef0818f2d6b5c3a86c560
Alexander Ildiyarov <alexvancasper@gmail.com>
2023-12-05 20:35:12 +0000

    HW05 fixed comments

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -4,77 +4,35 @@ import (
 	"context"
 	"errors"
 	"sync"
-	"time"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-type ErrCounter struct {
-	mx sync.Mutex
-	m  int32
-}
-
-func (c *ErrCounter) Reset() {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	c.m = 0
-}
-
-func (c *ErrCounter) Inc() {
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	c.m++
-}
-
-// Compare - возвращает true, если количество ошибок больше заданного числа, иначе false
-// Всегда возвращает false если n<0,т.е. игнорирование ошибок.
-func (c *ErrCounter) Compare(n int32) bool {
-	if n < 0 {
-		return false
-	}
-	c.mx.Lock()
-	defer c.mx.Unlock()
-	return c.m >= n
-}
-
-var errCount ErrCounter
-
 // Run starts tasks in n goroutines
 // and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	var wg sync.WaitGroup
-	errCount.Reset()
-	job := make(chan Task, n/2)
+	var errCount int32
+	atomic.StoreInt32(&errCount, 0)
+
+	job := make(chan Task, n)
 	ctx, ctxCancel := context.WithCancel(context.Background())
 
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(ctx, &wg, job)
+		go worker(ctxCancel, &wg, job, &errCount, int32(m))
 	}
 
 	wg.Add(1)
 	go writeTask(ctx, &wg, job, tasks)
 
-	go func(ctx context.Context, ctxCancel context.CancelFunc) {
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				if errCount.Compare(int32(m)) {
-					ctxCancel()
-					return
-				}
-			}
-		}
-	}(ctx, ctxCancel)
-
 	wg.Wait()
 	ctxCancel()
 
-	if errCount.Compare(int32(m)) {
+	if m >= 0 && atomic.LoadInt32(&errCount) >= int32(m) {
 		return ErrErrorsLimitExceeded
 	}
 
@@ -88,25 +46,21 @@ func writeTask(ctx context.Context, wg *sync.WaitGroup, job chan<- Task, tasks [
 		select {
 		case <-ctx.Done():
 			return
-		default:
-			job <- task
+		case job <- task:
 		}
-		time.Sleep(1 * time.Millisecond)
 	}
 }
 
-func worker(ctx context.Context, wg *sync.WaitGroup, job <-chan Task) {
+func worker(ctxCancel context.CancelFunc, wg *sync.WaitGroup, job <-chan Task, errCounter *int32, maxError int32) {
 	defer wg.Done()
 	for task := range job {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			err := task()
-			if err != nil {
-				errCount.Inc()
+		err := task()
+		if err != nil && maxError >= 0 {
+			atomic.AddInt32(errCounter, 1)
+			if atomic.LoadInt32(errCounter) >= maxError {
+				ctxCancel()
+				return
 			}
 		}
-		time.Sleep(1 * time.Millisecond)
 	}
 }
